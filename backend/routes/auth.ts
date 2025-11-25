9d25dfd42b65f6d37935681f08d43bc024147dbd
emal <emalhasitha@proton.me>
2025-11-25 06:21:08 +0530

    refactor: rename auth handlers import

--- backend/routes/auth.ts
+++ backend/routes/auth.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import * as auth from "@/handlers/auth.js";
+import * as handlers from "@/handlers/auth.js";
 
 const router = Router();
 
-router.get("/github", auth.githubSignin);
-router.get("/github/callback", auth.githubCallback);
+router.get("/github", handlers.githubSignin);
+router.get("/github/callback", handlers.githubCallback);
 // router.get("/guest");
 
 export default router;
