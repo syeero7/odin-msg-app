394bc84e8354061fed746e8c0f6f7e8888e1e1ae
emal <emalhasitha@proton.me>
2025-11-25 21:26:14 +0530

    fix: connect group request handlers to routes

--- backend/routes/groups.ts
+++ backend/routes/groups.ts
@@ -1,15 +1,16 @@
 import { Router } from "express";
+import * as handlers from "@/handlers/groups.js";
 
 const router = Router();
 
-router.get("/");
-router.post("/");
+router.get("/", handlers.getUserGroups);
+router.post("/", handlers.createGroup);
 
-router.get("/:groupId");
+router.get("/:groupId", handlers.getGroupById);
 
-router.get("/:groupId/members");
-router.get("/:groupId/nonmembers");
-router.put("/:groupId/members/:userId"); // action="remove" || "add"
-router.delete("/:groupId");
+router.get("/:groupId/members", handlers.getGroupMembers);
+router.get("/:groupId/nonmembers", handlers.getGroupNonmembers);
+router.put("/:groupId/members/:userId", handlers.updateGroupMembers); // action="remove" || "add"
+router.delete("/:groupId", handlers.deleteGroupById);
 
 export default router;
