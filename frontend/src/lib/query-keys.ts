f97f2c13a0e46014005809d68baf1efd335bf613
emal <emalhasitha@proton.me>
2025-12-11 14:58:29 +0530

    feat: update query keys

--- frontend/src/lib/query-keys.ts
+++ frontend/src/lib/query-keys.ts
@@ -1,14 +1,17 @@
 import type { QueryKey } from "@tanstack/react-query";
 
-export const CURRENT_USER: QueryKey = ["currentUser"];
+export const CURRENT_USER: QueryKey = ["current_user"];
 export const GROUPS: QueryKey = ["groups"];
 export const USERS: QueryKey = ["users"];
 export const USER = (userId: number | string): QueryKey => {
   return ["user", `${userId}`];
 };
 export const DIRECT_MSG = (userId: number | string): QueryKey => {
-  return ["direct", `${userId}`];
+  return ["direct_msg", `${userId}`];
+};
+export const GROUP = (groupId: number | string): QueryKey => {
+  return ["group", `${groupId}`];
 };
 export const GROUP_MSG = (groupId: number | string): QueryKey => {
-  return ["direct", `${groupId}`];
+  return ["group_msg", `${groupId}`];
 };
